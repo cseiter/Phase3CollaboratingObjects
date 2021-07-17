90d4319c301142805b7caccad09d76f651776fda
chris <cseiter@gmail.com>
2021-07-17 17:11:05 -0400

    finally got the tests passed on the files method

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class MP3Importer
 
     attr_accessor :path
@@ -7,6 +9,11 @@ class MP3Importer
     end
 
     def files
+        #file_list = Dir.glob(File.join("**","*.mp3"), base: "#{path}")
+        #binding.pry 
+        #@files = Dir.glob("#{path}/*.mp3")
+        #@files ||= @files.collect {|ind_file_names| ind_file_names.sub!("#{path}/","")}
+        @files ||= Dir.glob("#{path}/*.mp3").collect {|ind_file_names| ind_file_names.gsub("#{path}/","")}
     end
 
     def import
