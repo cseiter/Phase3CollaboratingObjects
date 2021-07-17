fdcb71dcd3afa9f2382ffc7c86c93f96dbc588cb
chris <cseiter@gmail.com>
2021-07-17 17:31:47 -0400

    all tests passed

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
 
     attr_accessor :name, :artist
@@ -18,9 +20,17 @@ class Song
     end
 
     def self.new_by_filename(filename)
+        song_artist = filename.split(" - ")[0]
+        song_title = filename.split(" - ")[1]
+        new_song = self.new(song_title)
+        new_song.artist_name = song_artist
+        new_song
+        #binding.pry
     end
 
     def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)
     end
 
 end
