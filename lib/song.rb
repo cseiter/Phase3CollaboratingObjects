0452e59695c03cb3a1d08596fd3b0e0153828374
chris <cseiter@gmail.com>
2021-07-17 16:16:43 -0400

    create initial Song class and file

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name=name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.new_by_filename(filename)
+    end
+
+    def artist_name=(name)
+    end
+
+end
